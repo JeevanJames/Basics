63b8594893ffb389e18cd2178d6831c977edabb6
Jeevan James <jeevan.james@tavant.com>
2016-03-23 18:51:55 +0530

    Moved the existing solution to a legacy submodule to make way for the new .NET Core structure.

---
+++ legacy/SolutionInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jeevan James")]
+[assembly: AssemblyProduct("Basics for .NET")]
+[assembly: AssemblyCopyright("Copyright © 2015-2016 Jeevan James")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha06")]
---
+++ legacy/SolutionInfo_Tests.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jeevan James")]
+[assembly: AssemblyProduct("Basics for .NET")]
+[assembly: AssemblyCopyright("Copyright © 2015-2016 Jeevan James")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
