bda5345a4822c11696c8d40fcc23a2d50a80d95e
Jeevan James <jeevan.james@tavant.com>
2016-03-28 14:37:48 +0530

    Removed the portable projects for now.

--- legacy/Basics.1.2/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Resources;
-
-[assembly: AssemblyTitle("Basics")]
-[assembly: AssemblyDescription("Basics core package containing common abstractions and models.")]
-[assembly: NeutralResourcesLanguage("en")]
