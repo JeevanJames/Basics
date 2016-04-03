b42c00e86b657964377acfd5c72087e65b0f0423
Jeevan James <jeevanjj@yahoo.com>
2016-04-04 04:03:04 +0530

    Moved BasicsClaimTypes.cs to the core library.

--- legacy/Basics.AspNet.WebApi/BasicsClaimTypes.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Basics.AspNet.WebApi
-{
-    public static class BasicsClaimTypes
-    {
-        public const string ClaimTypeNamespace = "http://schemas.jeevanjames.com/ws/2015/12/identity/claims";
-
-        public const string Permission = ClaimTypeNamespace + "/permission";
-    }
-}
---
+++ legacy/Basics/SecurityExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Claims;
+
+namespace Basics
+{
+    public static class BasicsClaimTypes
+    {
+        public const string ClaimTypeNamespace = "https://www.nuget.org/packages/Basics/ws/2015/12/identity/claims";
+
+        public const string Permission = ClaimTypeNamespace + "/permissions";
+    }
+
+    public static class SecurityExtensions
+    {
+        public static void Demand(this ClaimsPrincipal principal, string requiredPermission)
+        {
+            Demand(principal, perms => perms.ContainsKey(requiredPermission));
+        }
+
+        public static void DemandAny(this ClaimsPrincipal principal, params string[] requiredPermissions)
+        {
+            Demand(principal, perms => perms.Any(perm => requiredPermissions.Any(rp => rp.Equals(perm.Key, StringComparison.Ordinal))));
+        }
+
+        public static void DemandAll(this ClaimsPrincipal principal, params string[] requiredPermissions)
+        {
+            Demand(principal, perms => perms.All(perm => requiredPermissions.Any(rp => rp.Equals(perm.Key, StringComparison.Ordinal))));
+        }
+
+        private static void Demand(ClaimsPrincipal principal, Func<IDictionary<string, string>, bool> predicate)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+            Claim permissionClaim = principal.FindFirst(BasicsClaimTypes.Permission);
+            if (permissionClaim == null)
+                throw new SecurityException("You do not have permissions to perform this operation.");
+            if (!predicate(permissionClaim.Properties))
+                throw new SecurityException("You do not have permissions to perform this operation.");
+        }
+    }
+}
